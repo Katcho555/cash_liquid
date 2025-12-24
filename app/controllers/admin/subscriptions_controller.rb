42532f495312dffebe71c8c160a88dd4afd3499a
matinou-github <hadilouidrissou@gmail.com>
2025-12-24 11:53:41 +0100

    fix subscription bug 1

--- app/controllers/admin/subscriptions_controller.rb
+++ app/controllers/admin/subscriptions_controller.rb
@@ -3,13 +3,19 @@ class Admin::SubscriptionsController < Admin::BaseController
 
   # Créditer tous les dividendes dus pour tous les utilisateurs
   def credit_all_pending
-    total_credits = 0
-    Subscription.where(status: "payé").find_each do |sub|
+  total_credits = 0
+
+  Subscription
+    .where(status: "payé")
+    .where.not(product_id: nil)   # <-- filtrage important
+    .find_each do |sub|
       total_credits += sub.credit_all_due_dividends!
     end
-    flash[:success] = "Tous les dividendes non crédités ont été versés : #{total_credits} FCFA"
-    redirect_to admin_subscriptions_path
-  end
+
+  flash[:success] = "Tous les dividendes non crédités ont été versés : #{total_credits} FCFA"
+  redirect_to admin_subscriptions_path
+end
+
 
   # Créditer une seule souscription via bouton
   def credit_dividend
@@ -21,7 +27,8 @@ class Admin::SubscriptionsController < Admin::BaseController
 
   # Affichage de toutes les souscriptions
   def index
-    @subscriptions = Subscription.includes(:user, :product).order(created_at: :desc)
+
+    @subscriptions = Subscription.includes(:user, :product).where.not(product_id: nil).order(created_at: :desc)
   end
 
 end
